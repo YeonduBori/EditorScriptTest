209d2672173eba23682c969d0cf57ccd264728d5
dhshin <dhshin@virnect.com>
2020-07-21 17:14:08 +0900

    TagName에 Tag를 적고 Find Tag Object 버튼을 누르면 해당하는 오브젝트가 찾아지는 기능 구현

---
+++ TestRoom/Assets/Editor/TagFinderGUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TagObjectFinder))]
+public class TagFinderGUI : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        TagObjectFinder finder = (TagObjectFinder)target;
+        
+        if(GUILayout.Button("Find Tag Object"))
+        {
+            finder.FindObjects();
+        }
+    }
+}
---
+++ TestRoom/Assets/Scipts/TagObjectFinder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagObjectFinder : MonoBehaviour
+{
+    public GameObject[] friends;
+
+    public string tagName;
+
+    public void FindObjects()
+    {
+        friends = GameObject.FindGameObjectsWithTag(tagName);
+    }
+}
